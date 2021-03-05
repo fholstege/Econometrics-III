7efed1b56c03da5427408fc5f9091d0d26f2554d
Walter Verwer <walterverwer@gmail.com>
2021-03-05 15:04:41 +0100

    minor typo

--- part-1/ti_ect_iii_p1.py
+++ part-1/ti_ect_iii_p1.py
@@ -5,7 +5,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
-from statsmodels.tsa.ar_model import AR
 from statsmodels.tsa.ar_model import AutoReg
 
 # Data
